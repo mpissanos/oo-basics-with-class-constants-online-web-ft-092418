3b431bbdcb5815050742d3d28117f3303630ce0e
Manny Pissanos <mpissano@gmail.com>
2018-09-27 19:53:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS
+    if BRANDS.include?(@brand)
+    end
     BRANDS << @brand
   end
+end
 
   def cobble
     self.condition = "new"
